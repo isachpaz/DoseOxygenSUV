53a2ec21aacd6856200f236fc73658f0fae34c07
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2021-02-09 10:58:58 +0100

    plot dose modification factor vs. pO2

---
+++ main.py
@@ -0,0 +1,34 @@
+from dosemodificationfactor import DoseModificationFactor
+from oxygenmodificationfactor import OxygenModificationFactor
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_omf():
+    omf = OxygenModificationFactor(max_oer=3.0, k_mmHg=2.5)
+    po2line = np.linspace(start=0.0, stop=20.0, num=400)
+    plt.plot(po2line, omf.calculate(po2line))
+    plt.show()
+
+
+def plot_dmf():
+    dmf = DoseModificationFactor(max_oer=3.0, k_mmHg=2.5)
+    po2line = np.linspace(start=0.0, stop=10.0, num=400)
+    plt.plot(po2line, dmf.calculate(po2line), linewidth=2)
+    plt.xlabel("$pO_2$ (mmHg)",  fontsize=16)
+    plt.ylabel("Dose modification factor",  fontsize=16)
+
+    plt.tick_params(labelsize=14)
+
+    # Show the major grid lines with dark grey lines
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
+    # Show the minor grid lines with very faint and almost transparent grey lines
+    plt.minorticks_on()
+    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+    plt.show()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # plot_omf()
+    plot_dmf()
