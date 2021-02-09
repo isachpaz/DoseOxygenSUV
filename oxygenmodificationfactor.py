3ea90653bb8f09ffb50ab50e0177a5a9aa90b22d
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2021-02-09 10:57:47 +0100

    oxygen modification factor added

---
+++ oxygenmodificationfactor.py
@@ -0,0 +1,9 @@
+class OxygenModificationFactor():
+
+    def __init__(self, max_oer, k_mmHg) -> None:
+        super().__init__()
+        self.k_mmHg = k_mmHg
+        self.max_oer = max_oer
+
+    def calculate(self, po2):
+        return (self.max_oer * po2 + self.k_mmHg) / (self.max_oer * (po2 + self.k_mmHg))
