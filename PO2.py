8b2efd860786d88b400e66fb0a506e5f0327c5e5
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2021-03-18 14:15:20 +0100

    Updated

--- PO2.py
+++ PO2.py
@@ -11,10 +11,9 @@ class PO2:
     def calculate_uptake_from_po2(self, po2):
         return self.A - (self.B*po2 / (self.C + po2))
 
-    def calculate_normalized_uptake_from_po2(self, po2):
-        factor60mmHg = self.calculate_uptake_from_po2(60)
-        po2 = (po2/factor60mmHg) - 1.0
-        return self.A - (self.B*po2 / (self.C + po2))
+    def calculate_normalized_uptake_from_po2(self, po2, to_normalization_point):
+        factor_in_mm_hg = self.calculate_uptake_from_po2(to_normalization_point)
+        return (self.A - (self.B*po2 / (self.C + po2)))/factor_in_mm_hg
 
     def __repr__(self) -> str:
         return f"A={self.A}, B={self.B}, C={self.C}"
--- main.py
+++ main.py
@@ -74,26 +74,78 @@ def plot_normalized_uptake():
     B = 10.7
     C = 2.5
     po2func = PO2(A, B, C)
-    print(f"Uptake = {po2func.calculate_uptake_from_po2(60)} for 60 mmHg")
-    spo2s = np.linspace(start=.1, stop=60, num=4000)
-    uptakes = po2func.calculate_normalized_uptake_from_po2(spo2s)
-    plt.plot(spo2s, uptakes)
-    #plt.xscale('log')
-    plt.ylim(0, 20, 1)
-    # plt.xlim(0, 2000)
+    print(f"Uptake for pO2=60mmHg  = {po2func.calculate_uptake_from_po2(60)}")
+    print(f"Uptake for pO2=38mmHg  = {po2func.calculate_uptake_from_po2(38)}")
+    spo2s = np.linspace(start=0, stop=200, num=40000)
+    normalization_point = 38
+    uptakes = po2func.calculate_normalized_uptake_from_po2(spo2s, normalization_point)
+    ax = plt.subplot(111)
+    ax.plot(spo2s, uptakes)
+
+    plt.xscale('log')
+    # plt.ylim(0, np.max(uptakes) + 1, 1)
     plt.grid(b=True, which='major', color='#666666', linestyle='-')
     plt.minorticks_on()
     plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
     plt.tick_params(labelsize=14)
-    plt.yticks(np.arange(0, 20 + 1, 1.0))
+    plt.yticks(np.arange(0, np.max(uptakes) + 1, 1.0))
     plt.xlabel("$pO_2$ (mmHg)", fontsize=16)
-    plt.ylabel("Uptake (normalized to 60mmHg)", fontsize=16)
+    plt.ylabel(f"Uptake (normalized to {round(normalization_point)}mmHg)", fontsize=16)
 
-    po2Points = [0.0, 0.76, 3.8, 38, 152]
-    uptakePoints = [10.9, 7.7, 5.6, 1, 0.5]
+    po2Points = [0.0076, 0.76, 3.8, 38, 152]
+    uptakePoints = np.array([10.9, 7.7, 5.6, 1, 0.5])
 
-    #plt.scatter(po2Points, uptakePoints)
+    colors = ['b', 'g', 'r', 'b', 'r']
 
+    ax.scatter(po2Points, uptakePoints / po2func.calculate_uptake_from_po2(normalization_point), marker='x',
+                c=colors[1], label='normalized data')
+
+    ax.scatter(po2Points, uptakePoints,  label='initial data', c=colors[2])
+
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+def plot_normalized_uptake_for_38_and_60_mmhg():
+    A = 10.9
+    B = 10.7
+    C = 2.5
+    po2func = PO2(A, B, C)
+    f60 = po2func.calculate_uptake_from_po2(60)
+    f38 = po2func.calculate_uptake_from_po2(38)
+    spo2s = np.linspace(start=8, stop=1000, num=40000)
+
+    normalization_point = 38
+    uptakes38 = po2func.calculate_normalized_uptake_from_po2(spo2s, 38)
+    uptakes60 = po2func.calculate_normalized_uptake_from_po2(spo2s, 60)
+    ax = plt.subplot(111)
+    ax.plot(spo2s, uptakes60, '--', c='b', label="normalized to 60 mmHg")
+    ax.plot(spo2s, uptakes38, '-', c='g', label="normalized to 38 mmHg")
+
+
+    plt.xscale('log')
+
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
+    plt.minorticks_on()
+    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+    plt.tick_params(labelsize=14)
+    # plt.yticks(np.arange(0, np.max(uptakes60) + 1, 1.0))
+    plt.yticks(np.arange(0, 4 + 1, 0.5))
+    #plt.xticks(np.arange(10, 1100, 100))
+    plt.xlabel("$pO_2$ (mmHg)", fontsize=16)
+    plt.ylabel(f"Normalized Uptake", fontsize=16)
+
+    po2Points = [ 38, 152]
+    uptakePoints = np.array([ 1, 0.5])
+
+    colors = ['b', 'g', 'r', 'b', 'r']
+
+    ax.scatter(po2Points, uptakePoints / po2func.calculate_uptake_from_po2(38), marker='x',
+                c=colors[1], label='normalized data to 38mmHg')
+
+    ax.scatter(po2Points, uptakePoints / po2func.calculate_uptake_from_po2(60),  label='normalized data to 60mmHg', c=colors[0])
+
+    plt.legend(loc='upper right')
     plt.show()
 
 
@@ -117,6 +169,7 @@ def plot_dmf():
 if __name__ == '__main__':
     # plot_omf()
     # plot_dmf()
-    #plot_po2()
-    plot_uptake()
-    #plot_normalized_uptake()
+    # plot_po2()
+    # plot_uptake()
+    plot_normalized_uptake()
+    # plot_normalized_uptake_for_38_and_60_mmhg()
